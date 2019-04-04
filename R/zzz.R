f16ec322a25da1d8f1380314f76561846998a74f
Aaron Gullickson <aarong@uoregon.edu>
2019-04-04 13:27:33 -0700

    added additional 513 datasets

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+.onLoad <- function(libname, pkgname){
+  ## load data into the environment so students
+  ## can see it
+  data(titanic)
+  data(politics)
+  data(sex)
+  data(movies)
+  data(crimes)
+  data(addhealth)
+  data(popularity)
+  data(espinganderson)
+  data(nyc)
+  data(earnings)
+  data(wbreport)
+}
